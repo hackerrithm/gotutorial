6ee08b5657f3e13421c7338071fa4a965d7ca535
Kemar Galloway <kemargalloway@gmail.com>
2017-02-13 12:30:57 -0500

    Go Programming
    Learning Go!!!

---
+++ littletest/src/github.com/hackerrithm/addtwo/addtwo.go
@@ -0,0 +1,8 @@
+// Package stringutil contains utility functions for working with strings.
+package addtwo
+
+// Reverse returns its argument string reversed rune-wise left to right.
+func Aeverse(s int) int {
+	output := 2 + 3
+	return output
+}
---
+++ littletest/src/github.com/hackerrithm/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hackerrithm/stringutil"
+  "github.com/hackerrithm/addtwo"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("!yawollaG ,rameK"))
+  fmt.Println(addtwo.Aeverse(8))
+}
